85691dfb8a5fd5aa4957f4fbde6e48ba791c712b
Karson Tran <karsont@uci.edu>
2023-01-20 15:02:11 -0800

    remove start function for game

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -7,11 +7,6 @@ public class BulletBehavior: MonoBehaviour
     public float BulletSpeed;
     public float maxY;
     
-    // Start is called before the first frame update
-    void Start()
-    
-    }
-
     // Update is called once per frame
     void Update()
     {
