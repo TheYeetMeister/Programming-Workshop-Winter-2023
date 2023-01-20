74965b585788201aa5eb9094ebc22f20263a05f6
Karson Tran <karsont@uci.edu>
2023-01-20 15:01:05 -0800

    create new public function for script

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class BulletBehavior: MonoBehaviour
 {
     public float BulletSpeed;
+    public float maxY;
+    
     // Start is called before the first frame update
     void Start()
-    {
-        
+    
     }
 
     // Update is called once per frame
