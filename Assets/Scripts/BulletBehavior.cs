5fc18de24325ba28a306f55abafd90d170ec961d
Karson Tran <karsont@uci.edu>
2023-01-20 14:59:12 -0800

    create new prefab for bullet, add script to prefab

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BulletBehavior: MonoBehaviour
 {
     public float BulletSpeed;
     // Start is called before the first frame update
